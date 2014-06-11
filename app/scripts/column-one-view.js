ffc2f976cba7d79ec7abd7242e95717cb01af35b
luke olson <luukeout@gmail.com>
2014-06-11 07:45:15 -0400

    data in column one with buttons

--- app/scripts/column-one-view.js
+++ app/scripts/column-one-view.js
@@ -2,11 +2,28 @@
 
 var ColumnOneView = Backbone.View.extend({
 
+	className: 'dataView',
+
+	template: _.template($('.data-template').text()),
+
+	events: {
+		"click .move-button" : "",
+		"click .copy-button" : ""
+	},
+
 	initialize: function(){
+
+		$('.column-one').append(this.el);
+		this.render()
 
 	},
 
 	render: function(){
+
+		var renderedTemplate = this.template(this.model.attributes)
+		// here the el is getting the html of renderedTemplate added to it.
+		this.$el.html(renderedTemplate)
+	
 
 	},
 
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+"use strict";
+
+var newData = new DataCollectionOne();
+
+
+newData.fetch().done(function(){
+	newData.each(function(data){
+		new ColumnOneView({model: data});
+	})
+
+});
